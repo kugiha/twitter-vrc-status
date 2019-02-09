211d7cc6bde8614ccafacd91318d30ae6410a8a7
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-02-09 21:07:29 +0900

    Add update_twitter_profile

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # twitter-vrc-status
 Update your twitter location or name according to your VRC status
+## Usage
+Set these environment variables  
+- twitter_consumer_key
+- twitter_consumer_secret
+- twitter_oauth_token
+- twitter_oauth_token_secret
+
 ## Links
 The following links help development of this repo.
 - https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,2 +1,40 @@
+import os
+from requests_oauthlib import OAuth1Session
+
+offline_location = 'リアルワールド出張中'
+private_location = "(private in VRC)"
+
+name = {
+    'name_template': '柊 釘葉{status}',
+    'online_status': '✅',
+    'offline_status': '📴'
+}
+
 def lambda_handler(event, context):
+    is_online, location = get_vrchat_status()
+    update_twitter_profile(is_online, location)
+
+def update_twitter_profile(is_online, location):
+    """
+    Parameters
+    ----------
+    is_online : boolean
+    location : str
+        String to be set as user's location with no formatting.
+        This function is not responsible for masking private location or offline status.
+    """
+    twitter = OAuth1Session(
+        os.environ['consumer_key'],
+        os.environ['consumer_secret'],
+        os.environ['oauth_token'],
+        os.environ['oauth_token_secret']
+    )
+    params = {
+        'name': name['name_template'].format(status = name[('on' if is_online else 'off') + 'line_status']),
+        'location': location
+    }
+    twitter.post('https://api.twitter.com/1.1/account/update_profile.json', params=params)
     return "lambda test"
+
+def get_vrchat_status():
+    pass
